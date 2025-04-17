c7fcc64c13d058096de9ca85d114da4a1b9219d3
DeadlySage <adrian.hartanto159@gmail.com>
2025-04-17 11:12:50 -0700

    added status and messages to returns in json

--- server/index.js
+++ server/index.js
@@ -12,7 +12,10 @@ app.use(require("morgan")("dev"));
 
 // app routes
 app.get("/", (req, res) => {
-  res.status(200).json({ message: "API is working" });
+  res.status(200).json({ 
+    status: 200,
+    message: "API is working" 
+  });
 });
 
 app.get('/api/flavors', async (req ,res, next) => {
@@ -23,7 +26,10 @@ app.get('/api/flavors', async (req ,res, next) => {
 
     const response = await client.query(SQL);
 
-    res.send(response.rows)
+    res.status(200).json({
+      status: 200,
+      data: response.rows
+    })
   } catch (error) {
     next(error);
   }
@@ -40,7 +46,10 @@ app.get('/api/flavors/:id', async (req, res, next) => {
 
     const response = await client.query(SQL, [id]);
     
-    res.send(response.rows)
+    res.status(200).json({
+      status: 200,
+      data: response.rows[0]
+    })
   } catch (error) {
     next(error);
   }
@@ -58,7 +67,10 @@ app.post('/api/flavors', async (req, res, next) => {
     `
     const response = await client.query(SQL, [name, is_favorite]);
 
-    res.send(response.rows[0])
+    res.status(201).json({
+      status: 201,
+      data: response.rows[0]
+    });
   } catch (error) {
     next(error);
   }
@@ -81,16 +93,22 @@ app.delete('/api/flavors/:id', async (req, res, next) => {
 app.put('/api/flavors/:id', async (req, res, next) => {
   try {
     const id = req.params.id;
+    const name = req.body.name;
     const is_favorite = req.body.is_favorite;
     
     const SQL = `
       UPDATE flavors 
-      SET is_favorite = $2, 
+      SET is_favorite = COALESCE($3, is_favorite),
+        name = COALESCE($2, name), 
         updated_at = now()
       WHERE id = $1;
     `
-    const response = await client.query(SQL, [id, is_favorite]);
-    res.send(response.rows[0]);
+    
+    const response = await client.query(SQL, [id, name, is_favorite]);
+    res.status(201).json({
+      status: 201,
+      message: `succesfully updated flavor #${id}`
+    });
   } catch (error) {
     next(error);
   }
